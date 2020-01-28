590c0c3d85478f1a259ffcf5edc13433fa1b5f90
ABSphreak <absphreak@outlook.com>
2020-01-28 20:18:17 +0530

    Completing with Redux

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,25 @@
 import React, { Component } from "react";
-import axios from "axios";
+// import axios from "axios";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import Pokeball from "../pokeball.png";
 
 class Home extends Component {
-  state = {
-    posts: []
-  };
+  // state = {
+  //   posts: []
+  // };
 
-  componentDidMount() {
-    axios.get("https://jsonplaceholder.typicode.com/posts").then(res => {
-      this.setState({
-        posts: res.data.slice(0, 10)
-      });
-    });
-  }
+  // componentDidMount() {
+  //   axios.get("https://jsonplaceholder.typicode.com/posts").then(res => {
+  //     this.setState({
+  //       posts: res.data.slice(0, 10)
+  //     });
+  //   });
+  // }
 
   render() {
-    const { posts } = this.state;
+    // const { posts } = this.state;
+    const { posts } = this.props;
     const postList = posts.length ? (
       posts.map(post => {
         return (
@@ -44,4 +46,10 @@ class Home extends Component {
   }
 }
 
-export default Home;
+const mapStateToProps = state => {
+  return {
+    posts: state.posts
+  };
+};
+
+export default connect(mapStateToProps)(Home);
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,25 +1,37 @@
 import React, { Component } from "react";
-import axios from "axios";
+// import axios from "axios";
+import { connect } from "react-redux";
+import { deletePost } from "../actions/postActions";
 
 class Post extends Component {
-  state = {
-    post: null
+  // state = {
+  //   post: null
+  // };
+
+  // componentDidMount() {
+  //   let id = this.props.match.params.post_id;
+  //   axios.get("https://jsonplaceholder.typicode.com/posts/" + id).then(res => {
+  //     this.setState({
+  //       post: res.data
+  //     });
+  //   });
+  // }
+
+  handleClick = () => {
+    this.props.deletePost(this.props.post.id);
+    this.props.history.push("/");
   };
 
-  componentDidMount() {
-    let id = this.props.match.params.post_id;
-    axios.get("https://jsonplaceholder.typicode.com/posts/" + id).then(res => {
-      this.setState({
-        post: res.data
-      });
-    });
-  }
-
   render() {
-    const post = this.state.post ? (
+    const post = this.props.post ? (
       <div className="post">
-        <h4 className="center">{this.state.post.title}</h4>
-        <p>{this.state.post.body}</p>
+        <h4 className="center">{this.props.post.title}</h4>
+        <p>{this.props.post.body}</p>
+        <div className="center">
+          <button className="btn grey" onClick={this.handleClick}>
+            Delete Post
+          </button>
+        </div>
       </div>
     ) : (
       <div className="center">Loading ...</div>
@@ -28,4 +40,19 @@ class Post extends Component {
   }
 }
 
-export default Post;
+const mapStateToProps = (state, ownProps) => {
+  let id = ownProps.match.params.post_id;
+  return {
+    post: state.posts.find(post => post.id == id)
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    deletePost: id => {
+      dispatch(deletePost(id));
+    }
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Post);
